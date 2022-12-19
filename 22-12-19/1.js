cff5e1c83412c56669b6b07c4f600ecc1d290a5f
Amartan <tsoojamarbayasgalan@gmail.com>
2022-12-19 12:23:52 +0800

    22-12-19

---
+++ 22-12-19/1.js
@@ -0,0 +1,20 @@
+let garchig = document.getElementById("garchig")
+garchig.innerHTML = "Bi ta bvhendee Todo List App Hiij vzvvlie"
+
+// let container = document.getElementsByClassName("container");
+// container.style.display = "flex";
+
+// let taskContainer = document.getElementsByClassName("task-container")
+// taskContainer.style.display= flex;
+
+let btn = document.getElementById("myInput")
+
+function addTask(){
+    let jagsaalt = document.getElementById("jagsaalt");
+    jagsaalt.innerHTML = document.getElementById("myInput").value ;
+}
+
+
+let garchig2 = document.getElementById("garchig2")
+garchig2.innerHTML = "Ene bol jagsaalt"
+
