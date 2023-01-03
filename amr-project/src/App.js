4fff3837e0aee218e86bbf402d66f0fa38bddaf1
Amartan <tsoojamarbayasgalan@gmail.com>
2023-01-03 10:49:03 +0800

    23-01-03

--- amr-project/src/App.js
+++ amr-project/src/App.js
@@ -5,19 +5,75 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <div>
+          <p>sad zeppelins</p>
+
+        </div>
+        <div>
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer">PORTFOLIO</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"> BLOG</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"> CV</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"> STORE</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"> FREELANCE</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">ABOUT ME</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">CONTACT</a>
+        </div>
       </header>
+      <section>
+        <div>
+          <h1>Blog Posts</h1>
+          <h1>I think so, this is it.</h1>
+          <p>lorem60</p>
+          <div>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">TWITTER</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">LINKEDIN</a>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">MEDIUM</a>
+          </div>
+        </div>
+        <div>
+          <img src="./pic/Saly-16.png" alt="a" />
+        </div>
+      </section>
     </div>
   );
 }
