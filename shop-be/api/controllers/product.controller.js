dae6ae78f39c06f6ce6a6fcfebd36c736e1ce140
Amartan <tsoojamarbayasgalan@gmail.com>
2023-02-06 12:31:26 +0800

    23-02-06/s

---
+++ shop-be/api/controllers/product.controller.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const uuid = require("uuid");
+
+const dataFile = process.cwd() + "/data/product.json";
+
+exports.getAll = (request, response) => {
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return response.json({ status: false, message: readErr });
+    }
+
+    const savedData = JSON.parse(data);
+
+    return response.json({ status: true, result: savedData });
+  });
+};
+
+exports.create = (request, response) => {
+  const { productName, category, isSpecial, position } = request.body;
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return response.json({ status: false, message: readErr });
+    }
+
+    const parsedData = data ? JSON.parse(data) : [];
+
+    const newObj = {
+      id: uuid.v4(),
+      productName,
+      category,
+      isSpecial,
+      position,
+    };
+
+    parsedData.push(newObj);
+
+    fs.writeFile(dataFile, JSON.stringify(parsedData), (writeErr) => {
+      if (writeErr) {
+        return response.json({ status: false, message: writeErr });
+      }
+
+      return response.json({ status: true, result: parsedData });
+    });
+  });
+};
+
+exports.update = (request, response) => {
+  const { id } = request.params;
+  const { productName, category, isSpecial, position } = request.body;
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return response.json({ status: false, message: readErr });
+    }
+
+    const parsedData = JSON.parse(data);
+
+    const updateData = parsedData.map((productObj) => {
+      if (productObj.id == id) {
+        return { ...productObj, productName, category, isSpecial, position };
+      } else {
+        return productObj;
+      }
+    });
+
+    fs.writeFile(dataFile, JSON.stringify(updateData), (writeErr) => {
+      if (writeErr) {
+        return response.json({ status: false, message: writeErr });
+      }
+
+      return response.json({ status: true, result: updateData });
+    });
+  });
+};
+
+exports.delete = (request, response) => {
+  const { id } = request.params;
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return response.json({ status: false, message: readErr });
+    }
+
+    const parsedData = JSON.parse(data);
+
+    const deletedData = parsedData.filter((e) => e.id != id);
+
+    fs.writeFile(dataFile, JSON.stringify(deletedData), (writeErr) => {
+      if (writeErr) {
+        return response.json({ status: false, message: writeErr });
+      }
+
+      return response.json({ status: true, result: deletedData });
+    });
+  });
+};
--- shop-be/api/index.js
+++ shop-be/api/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const menuRouter = require("./routes/menu.route.js");
 const cateRouter = require("./routes/category.route.js");
+const productRouter = require("./routes/product.route.js");
 
 const fs = require("fs");
 
@@ -12,10 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", menuRouter);
-app.use("/api", cateRouter)
+app.use("/api", cateRouter);
+app.use("/api", productRouter);
 
 app.use("/api", (req, res) => {
-  res.json({ message: "Welcome To My Hearth" });
+  res.json({ message: "Welcome To My Heart" });
 });
 
-app.listen(port, () => console.log("The hearth is beating"));
+app.listen(port, () => console.log("The heart is beating"));
